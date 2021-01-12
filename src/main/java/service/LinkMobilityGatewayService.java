fd35a92eb7aadbb39d0e210027998653b6f23290
mhossain <mashfuk.hossain@crowdcode.io>
2021-01-12 14:41:59 +0100

    HSLB-152 added LinkedMobilityGateway

--- src/main/java/service/LinkMobilityGatewayService.java
+++ src/main/java/service/LinkMobilityGatewayService.java
@@ -14,13 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
+import java.net.*;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LinkMobilityGatewayService implements LinkMobilityGatewayInterface {
 
